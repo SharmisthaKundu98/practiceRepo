fd2e483d33871d07578c708ccb70684afe4da11b
rivuLayek <rivulayek15@gamil.com>
2023-01-08 22:03:56 +0530

    added sub function

--- foo.py
+++ foo.py
@@ -1,6 +1,19 @@
-def sum(a,b):
+def sum(a, b):
     return a+b
-print(sum(2,3))
-def multi(c,d):
+
+
+print(sum(2, 3))
+
+
+def multi(c, d):
     return c*d
-print(multi(4,6))
+
+
+print(multi(4, 6))
+
+
+def sub(a, b):
+    return a-b
+
+
+print(sub(5, 2))
